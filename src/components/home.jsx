158f5d3d764822c58c4dc6c9eec18c100fa930c2
Manpreet Singh <ms0014460@gmail.com>
2024-08-08 23:53:42 +0530

    Biosketch changes

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -27,46 +27,53 @@ function Home() {
         <div className="mb-5 flex flex-col gap-12  border-t-2 border-b-2 pt-10 pb-5">
           <h1 className=" text-[#9900ff] font-bold text-2xl">Biosketch:</h1>
           <p className="text-justify">
-            Dr. Hirak Mazumdar is a Biomedical Researcher and accomplished
-            Algorithm Developer specializing in Computer Vision, Image
-            Processing, and Complex Machine Learning Algorithms. He has
-            completed his PhD degree from Sogang University, South Korea jointly
-            with Samsung Mechatronics R&D, Suwon, Korea. After completing his
-            Doctorate he joined Cheongju National University, South Korea as a
-            Post-Doctoral fellow with the Ministry of Semiconductor
-            Design-funded project for building Software for Wafer testing during
-            the fabrication process using Machine Learning. Next, he moved to
-            the University of Houston, Texas, USA as a Post-Doctoral Scientist
-            Position where he worked with the Department of Energy (DOE) funded
-            Project for Groundwater Contamination Detection Using Machine
-            Learning. He is recognized as an academician, leader, and mentor in
-            his field. His current research interest includes developing
-            industrial big data analysis, intelligent medical sensor devices,
-            intelligent sensor manufacturing systems, and intelligent quality
-            assurance using complex Machine learning Algorithms.<br/><br/>In addition, he
-            has work experience with two top-tier companies (Samsung and
-            Hyundai) where he works with machine learning and data fault
-            analysis. Currently, his research interests include Nanotechnology,
-            Nano Biosensor Simulation, and Computational Biology with numerical
-            Simulation. He was also experienced with biomedical image analysis
-            and various cell cultures using Image Processing techniques.
+            Dr Sachin Mishra is a distinguished researcher and scientist with a
+            strong background in biosensors, nanotechnology, and materials
+            science. Holding a PhD in Biosensors from Linköping University,
+            Sweden, and the University of Free State, South Africa, he has held
+            research positions at esteemed institutions including Kwangwoon
+            University, South Korea, and the University of Rovira I Virgili,
+            Spain. Dr Mishra's research expertise encompasses the development of
+            electrochemical aptamer-based nano-biosensors, optical sensor
+            devices, and affinity-based analytical devices for point-of-care
+            applications. He boasts a strong publication record in top-tier
+            journals, including ACS Sensors, Biosensors and Bioelectronics, and
+            Nano Energy, and has authored several book chapters on MXenes and
+            nanotechnology for sustainable applications.<br/><br/>In addition to his
+            research experience, Dr Mishra has held leadership positions,
+            including Group Leader at VBRI Innovations Pvt. Ltd., India, and
+            Managing Editor at VBRI Press AB, Sweden, where he has successfully
+            managed teams and coordinated publishing projects. A skilled
+            communicator, Dr Mishra has presented his research at international
+            conferences and has mentored students at Kwangwoon University and
+            the University of Rovira I Virgili. Throughout his career, he has
+            demonstrated a strong commitment to advancing the field of
+            biosensors and nanotechnology, earning him the Goodfellow PhD
+            Student Award 2017.
           </p>
         </div>
         <div className="flex flex-col gap-2 py-6 border-b-2">
-            <p className="font-bold text-[#9900ff]">Professional Membership: </p>
-            <p className="font-semibold text-amber-800">1. IEEE Senior Member</p>
+          <p className="font-bold text-[#9900ff]">Professional Membership: </p>
+          <p className="font-semibold text-amber-800">1. IEEE Senior Member</p>
         </div>
         <div className="flex flex-col gap-2 py-6 border-b-2">
-            <p className="font-bold text-[#9900ff]">Journal Editor: </p>
-            <p className="font-semibold text-amber-800">1. Review Editor: Frontiers in Artificial Intelligence (Pattern Recognition)</p>
+          <p className="font-bold text-[#9900ff]">Journal Editor: </p>
+          <p className="font-semibold text-amber-800">
+            1. Review Editor: Frontiers in Artificial Intelligence (Pattern
+            Recognition)
+          </p>
         </div>
         <div className="flex flex-col gap-2 py-6 border-b-2">
-            <p className="font-bold text-[#9900ff]">Awards: </p>
-            <div>
-            <p className="font-semibold text-amber-800">1. Samsung Dream Scholarship</p>
+          <p className="font-bold text-[#9900ff]">Awards: </p>
+          <div>
+            <p className="font-semibold text-amber-800">
+              1. Samsung Dream Scholarship
+            </p>
             <p className="font-semibold text-amber-800">2. Fulbright Fellow</p>
-            <p className="font-semibold text-amber-800">3. STEM Ambassador, UHV, Texas, USA</p>
-            </div>
+            <p className="font-semibold text-amber-800">
+              3. STEM Ambassador, UHV, Texas, USA
+            </p>
+          </div>
         </div>
       </div>
     </div>
