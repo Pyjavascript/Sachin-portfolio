6fc909a14a94e6c4109de9e883a55e3df6f8b5cf
Manpreet Singh <ms0014460@gmail.com>
2024-08-29 17:36:00 +0530

    design changes

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -2,10 +2,23 @@ import React from "react";
 import Image from "./image";
 function Home() {
   return (
-  <div className=" h-screen lg:pt-5 lg:h-[85vh] lg:text-center lg:flex lg:items-center lg:justify-center">
+  <div className="h-auto lg:h-[85vh] lg:pt-20 lg:text-center lg:flex lg:items-start lg:justify-center">
     <Image/>
-    <p className="lg:relative lg:-top-44 lg:-right-14">I'm driving innovation in<br/>biosensors, and I love what i do.</p>
+    <div className="flex flex-col gap-40">
+    <p className="hidden lg:block pl-4 pt-10 lg:pt-0 text-center ">I'm driving innovation in biosensors, and I love what i do.</p>
+    <div className="card mt-24 flex gap-4 bg-white p-2 w-3/4 lg:w-auto lg:ml-0 ml-10 mb-2 lg:mb-0 lg:p-4 lg:py-5 rounded-lg">
+      <div className="flex flex-col items-start">
+        <h1 className="text-[#0D2F3F] font-medium text-1xl">Co-Authors</h1>
+        <img src="./assets/index.svg" alt="" />
+      </div>
+      <div className="flex flex-col items-start">
+        <h1 className="text-[#0D2F3F] font-medium text-1xl">H-Index</h1>
+        <h1 className="font-bold text-1xl text-[#286F6C]">8+</h1>
+      </div>
+    </div>
+    </div>
   </div>
+  
   )
 }
 
--- src/components/image.jsx
+++ src/components/image.jsx
@@ -3,25 +3,31 @@ import React from 'react'
 function Image() {
   return (
     <>
-    <div className="relative lg:flex gap-10 lg:items-start w-screen lg:w-auto">
-     <div className='flex lg:flex-row'>
+    <div className="lg:p-0 p-4 relative lg:items-start w-screen">
+     <div className='flex lg:flex-row lg:gap-64'>
      <div className='relative flex flex-col h-[55vh]'>
+     <div className='lg:w-80 lg:h-80 w-48 h-48'>
      <img src="./assets/name.svg" className=''/>
-     <img src="./assets/exp.svg" className='absolute bottom-0 left-0'/>
      </div>
-    <div className='relative'>
-    <img src="./assets/style.svg" className='-translate-y-1/2 -translate-x-1/2 absolute z-0 top-[60%] left-0'/>
-      <div className='lg:w-96 lg:h-96 w-52 h-52 linear-circle rounded-full flex justify-center items-center relative z-10'>
+     <div>
+     <img src="./assets/exp.svg" className='absolute -bottom-20 lg:-botton-auto left-0 '/>
+     </div>
+     </div>
+    <div className=' relative -translate-y-1/2 -translate-x-1/2 -bottom-72 right-[15%] lg:right-auto lg:-bottom-60'>
+    <img src="./assets/style.svg" className='-translate-y-1/2 -translate-x-1/2 absolute z-0 top-[15%] lg:rotate-0 rotate-45 lg:top-60 left-7 lg:w-auto w-48'/>
+      <div className=' lg:w-96 lg:h-96 w-52 h-52 linear-circle rounded-full flex justify-center items-center relative z-10'>
         <div className='overflow-hidden relative h-[97%] w-[97%] lg:h-[97%] lg:w-[97%] bg-white rounded-full '>
-            <img src="./assets/men.png" className='absolute -top-12' />
+            {/* <img src="./assets/men.png" className='absolute -top-12' /> */}
         </div>
-        <img src="./assets/head.png" className='w-[99%] absolute -top-12'/>
+        {/* <img src="./assets/head.png" className='w-[99%] absolute -top-12'/> */}
       </div>
     </div>
      </div>
+          <img src="./assets/sachin.png" className=' -rotate-90 absolute top-4 -left-8 lg:-top-5 lg:left-10 w-full h-full object-contain scale-150' />
     </div>
     </>
   )
 }
 
 export default Image
+
